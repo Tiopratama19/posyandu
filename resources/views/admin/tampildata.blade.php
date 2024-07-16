895a586f8ff9d60fd3e957772f458b77e5953b21
Tiopratama19 <tiomoreno36@gmail.com>
2024-07-17 05:20:07 +0700

    perubahan

--- resources/views/admin/tampildata.blade.php
+++ resources/views/admin/tampildata.blade.php
@@ -1,90 +1,89 @@
 @extends('Template.templateadmin')
 @section('content')
+    @push('title')
+        POSYANDU | Input Data Remaja
+    @endpush
 
-@push('title')
-POSYANDU | Input Data Remaja
-@endpush
+    @push('css')
+        <!-- DataTables -->
+        <!-- <link href="{{ asset('template1/themelibs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}"
+                rel="stylesheet" type="text/css" />
+            <link href="{{ asset('template1/themelibs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}"
+                rel="stylesheet" type="text/css" /> -->
+        <!-- Responsive datatable examples -->
+        <!-- <link href="{{ asset('template1/themelibs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}"
+                rel="stylesheet" type="text/css" /> -->
+    @endpush
 
-@push('css')
-<!-- DataTables -->
-<!-- <link href="{{ asset('template1/themelibs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}"
-        rel="stylesheet" type="text/css" />
-    <link href="{{ asset('template1/themelibs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}"
-        rel="stylesheet" type="text/css" /> -->
-<!-- Responsive datatable examples -->
-<!-- <link href="{{ asset('template1/themelibs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}"
-        rel="stylesheet" type="text/css" /> -->
-@endpush
-
-<div class="page-content">
-    <div class="container-fluid">
-        <!-- start page title -->
-        <div class="row">
-            <div class="col-12">
-                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0 font-size-18">Edit Data</h4>
-                    <div class="page-title-right">
-                        <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript: void(0);">Forms</a></li>
-                            <li class="breadcrumb-item active">Basic Elements</li>
-                        </ol>
+    <div class="page-content">
+        <div class="container-fluid">
+            <!-- start page title -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                        <h4 class="mb-sm-0 font-size-18">Edit Data</h4>
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">Forms</a></li>
+                                <li class="breadcrumb-item active">Basic Elements</li>
+                            </ol>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!-- end page title -->
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Silahkan Edit Data</h4>
+            <!-- end page title -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Silahkan Edit Data</h4>
 
-                        <span style="float:right">
-                            <a href="/admin/dataremaja" class="btn btn-danger">Kembali</a>
-                        </span>
-                    </div>
-                    <div class="card-body p-4">
+                            <span style="float:right">
+                                <a href="/admin/dataremaja" class="btn btn-danger">Kembali</a>
+                            </span>
+                        </div>
+                        <div class="card-body p-4">
 
-                        <div class="row">
-                            <form action="/admin/updatedata/{{ $data->id }}" method="POST">
-                                @csrf
-                                <div class="col-lg-12">
+                            <div class="row">
+                                <form action="/admin/updatedata/{{ $data->id }}" method="POST">
+                                    @csrf
                                     <div class="col-lg-12">
-                                        <div class="mb-3">
-                                            <label for="example-text-input" class="form-label">NIK</label>
-                                            <input class="form-control" name="NIK" type="text" value="{{$data->NIK}}"
-                                                id="example-text-input">
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="example-search-input" class="form-label">Nama</label>
-                                            <input class="form-control" name="Nama" type="text" value="{{$data->Nama}}"
-                                                id="example-search-input">
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="example-search-input" class="form-label">Tempat Lahir</label>
-                                            <input class="form-control" name="TempatLahir" type="text"
-                                                value="{{$data->TempatLahir}}" id="example-search-input">
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="example-email-input" class="form-label">Tanggal Lahir</label>
-                                            <input class="form-control" name="TanggalLahir" type="date"
-                                                value="{{$data->TanggalLahir}}" id="example-email-input">
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="example-tel-input" class="form-label">Jenis Kelamin</label>
-                                            <select class="form-select" name="JenisKelamin" aria-label="JenisKelamin">
-                                                @if($data->JenisKelamin == 'Laki-laki')
-                                                <option selected value="Laki-laki" > Laki-laki</option>
-                                                <option value="Perempuan" > Perempuan</option>
-                                                @elseif($data->JenisKelamin == 'Perempuan')
-                                                <option value="Laki-laki" > Laki-laki</option>
-                                                <option selected value="Perempuan" > Perempuan</option>
-                                                @endif
-                                            </select>
-                                            {{-- <input class="form-control" name="JenisKelamin" type="text" value="{{$data->JenisKelamin}}"
+                                        <div class="col-lg-12">
+                                            <div class="mb-3">
+                                                <label for="example-text-input" class="form-label">NIK</label>
+                                                <input class="form-control" name="NIK" type="text"
+                                                    value="{{ $data->NIK }}" id="example-text-input">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="example-search-input" class="form-label">Nama</label>
+                                                <input class="form-control" name="Nama" type="text"
+                                                    value="{{ $data->Nama }}" id="example-search-input">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="example-search-input" class="form-label">Tempat Lahir</label>
+                                                <input class="form-control" name="TempatLahir" type="text"
+                                                    value="{{ $data->TempatLahir }}" id="example-search-input">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="example-email-input" class="form-label">Tanggal Lahir</label>
+                                                <input class="form-control" name="TanggalLahir" type="date"
+                                                    value="{{ $data->TanggalLahir }}" id="example-email-input">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="example-tel-input" class="form-label">Jenis Kelamin</label>
+                                                <select class="form-select" name="JenisKelamin" aria-label="JenisKelamin">
+                                                    @if ($data->JenisKelamin == 'Laki-laki')
+                                                        <option selected value="Laki-laki"> Laki-laki</option>
+                                                        <option value="Perempuan"> Perempuan</option>
+                                                    @elseif($data->JenisKelamin == 'Perempuan')
+                                                        <option value="Laki-laki"> Laki-laki</option>
+                                                        <option selected value="Perempuan"> Perempuan</option>
+                                                    @endif
+                                                </select>
+                                                {{-- <input class="form-control" name="JenisKelamin" type="text" value="{{$data->JenisKelamin}}"
                                             id="example-tel-input"> --}}
-                                        </div>
-                                        {{-- <div class="mb-3">
+                                            </div>
+                                            {{-- <div class="mb-3">
                                                             <label for="example-email-input" class="form-label">Berat Badan</label>
                                                             <input class="form-control" name="BB" type="text" value="{{$data->BB}}"
                                         id="example-email-input">
@@ -116,38 +115,19 @@ POSYANDU | Input Data Remaja
                                         <input class="form-control" name="Anemia" type="text" value="{{$data->Anemia}}"
                                             id="example-email-input">
                                     </div> --}}
-                                    <span style="float:right">
-                                        <button type="Submit" class="btn btn-success">Submit</button>
-                                    </span>
-                                </div>
+                                            <span style="float:right">
+                                                <button type="Submit" class="btn btn-success">Submit</button>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        </form>
                     </div>
-                </div>
+                </div> <!-- end col -->
             </div>
-        </div> <!-- end col -->
+        </div> <!-- container-fluid -->
     </div>
-</div> <!-- container-fluid -->
-</div>
-<!-- End Page-content -->
-
-
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-6">
-                <script>
-                    Posyandu.write(new Date().getFullYear())
-
-                </script> © Admin.
-            </div>
-            <div class="col-sm-6">
-                <div class="text-sm-end d-none d-sm-block">
-                    Desain & Develop by <a href="#!" class="text-decoration-underline">Tio</a>
-                </div>
-            </div>
-        </div>
+    <!-- End Page-content -->
     </div>
-</footer>
-</div>
 @endsection
